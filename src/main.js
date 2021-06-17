76a3fddc9707aa75f8a07f50def032da970d3edc
Nicollas Feitosa <contatonicollasfeitosa@gmail.com>
2021-06-17 19:57:07 -0300

    Add vuex package

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import router from './router';
+import store from './store';
 import App from './App.vue';
 
 import './assets/css/tailwind.css';
@@ -8,5 +9,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
