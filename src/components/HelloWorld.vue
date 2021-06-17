6a670f6546f4df354feee752e32ecdebae084e88
Nicollas Feitosa <contatonicollasfeitosa@gmail.com>
2021-06-17 19:54:58 -0300

    Add Vue Router

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="hello">
         <h1>{{ msg }}</h1>
-        <p>
+        <p class="mt-10">
             For a guide and recipes on how to configure / customize this project,<br>
             check out the
             <a
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import router from './router';
 import App from './App.vue';
 
 import './assets/css/tailwind.css';
@@ -6,5 +7,6 @@ import './assets/css/tailwind.css';
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app');
